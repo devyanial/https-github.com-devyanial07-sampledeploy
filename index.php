cbca26fa030492af19f282f52747c0e95e87a79f
devyanial07 <34202154+devyanial07@users.noreply.github.com>
2017-12-03 12:56:26 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <h1>This is my PHP Test Application - version updated, with valid approval<h1>
 
 <?php
-echo "approval testing";
+echo "approval testing success";
 echo "Version:1.0.0 re-work-ver:approve";
 ?>
 
