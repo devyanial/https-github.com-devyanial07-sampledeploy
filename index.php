28947da265d34a6ec894e963d6dac98b59d8f5b9
devyanial07 <34202154+devyanial07@users.noreply.github.com>
2017-12-03 12:50:43 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 <html>
 <body>
 
-<h1>This is my PHP Test Application - version updated<h1>
+<h1>This is my PHP Test Application - version updated, with valid approval<h1>
 
 <?php
-echo "Version:1.0.0 re-work-ver";
+echo "Version:1.0.0 re-work-ver:approve";
 ?>
 
 </body>
